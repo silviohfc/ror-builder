d6ea0b0ba8d037734c55654d6cffabafd8b95ca1
Silvio Henrique <silviohfc@gmail.com>
2022-02-17 18:14:42 -0300

    refactor: change HoverCard to a component

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,73 +1,22 @@
-import { styled, keyframes } from '@stitches/react';
-import * as HoverCardPrimitive from '@radix-ui/react-hover-card';
+import { styled } from '@stitches/react';
 import { grayDark } from '@radix-ui/colors'
-import { ItemProps, ItemStat } from '../types/Item'
 
-const slideUpAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateY(2px)' },
-  '100%': { opacity: 1, transform: 'translateY(0)' },
-});
-
-const slideRightAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateX(-2px)' },
-  '100%': { opacity: 1, transform: 'translateX(0)' },
-});
-
-const slideDownAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateY(-2px)' },
-  '100%': { opacity: 1, transform: 'translateY(0)' },
-});
-
-const slideLeftAndFade = keyframes({
-  '0%': { opacity: 0, transform: 'translateX(2px)' },
-  '100%': { opacity: 1, transform: 'translateX(0)' },
-});
-
-const StyledContent = styled(HoverCardPrimitive.Content, {
-  borderRadius: 6,
-  padding: 20,
-  paddingTop: 4,
-  width: 300,
-  backgroundColor: 'white',
-  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-  '@media (prefers-reduced-motion: no-preference)': {
-    animationDuration: '300ms',
-    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
-    willChange: 'transform, opacity',
-    '&[data-state="open"]': {
-      '&[data-side="top"]': { animationName: slideDownAndFade },
-      '&[data-side="right"]': { animationName: slideLeftAndFade },
-      '&[data-side="bottom"]': { animationName: slideUpAndFade },
-      '&[data-side="left"]': { animationName: slideRightAndFade },
-    },
-  },
-});
-
-const StyledArrow = styled(HoverCardPrimitive.Arrow, {
-  fill: 'white',
-});
-
-export const HoverCard = HoverCardPrimitive.Root;
-export const HoverCardTrigger = HoverCardPrimitive.Trigger;
-export const HoverCardContent = StyledContent;
-export const HoverCardArrow = StyledArrow;
+import { HoverCard, HoverCardContent, HoverCardTrigger} from './HoverCard'
+import { ItemProps } from '../types/Item'
 
 const Table = styled('table', {
   width: '100%',
   border: 'solid 1px lightgray',
   borderRadius: 6,
 })
-
 const Td = styled('td', {
   textAlign: 'center'
 })
-
 const ItemIcon = styled('img', {
   display: 'block',
   width: 56,
   height: 56,
 })
-
 const ItemDivTrigger = styled('div', {
   display: 'flex',
   backgroundColor: grayDark.gray4,
@@ -119,7 +68,6 @@ export const Item = (props: ItemProps) => (
           }
         
       </Table>
-      <HoverCardArrow />
     </HoverCardContent>
   </HoverCard>
 );
