69f68c5619c335432d40f01435a539c92e881138
Silvio Henrique <silviohfc@gmail.com>
2022-02-17 16:47:59 -0300

    feat: add card hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Item } from "./components/Item"
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Item/>
+    </div>
+  )
 }
 
 export default App
